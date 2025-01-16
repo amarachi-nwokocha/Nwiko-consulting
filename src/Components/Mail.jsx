0379ac36087664273c2add437794848b26a3b156
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2025-01-16 16:50:02 +0300

    email changed

--- src/Components/Mail.jsx
+++ src/Components/Mail.jsx
@@ -6,7 +6,7 @@ const Mail = () => {
         <h2 className='text-blue text-center font-bold text-4xl mb-10'>Contact Us</h2>
         <p className='font-semibold text-2xl text-blue'>Partner with us to transform your project from concept to completion. Experience our expertise firsthand — a single trial will exceed your expectations</p>
        <button className='my-20'>
-       <a href="mailto:example@email.com?subject=Hello&body=I%20want%20to%20work%20with%20you." className='bg-blue w-10 h-10 p-4 rounded-md font-semibold text-2xl text-white border-2 border-solid border-yellow'>
+       <a href="mailto:pnwiko@nwikoconsulting.com?subject=Request%20for%20a%20meeting&body=I%20want%20to%20work%20with%20you." className='bg-blue w-10 h-10 p-4 rounded-md font-semibold text-2xl text-white border-2 border-solid border-yellow'>
     Send Mail
   </a>
        </button>
