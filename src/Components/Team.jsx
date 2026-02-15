8bd598db30a9991bdd27f679fa8361a08dfa9cc4
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2026-02-15 20:26:42 +0100

    added lco

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -4,8 +4,9 @@ import ceo from "../assets/person1.jpg";
 import coo from "../assets/person2.jpg";
 import cto from "../assets/cto.jpg";
 import cmo from "../assets/mau.jpg";
-import cfo from '../assets/alex.jpg'
-import sco from '../assets/sunny.png'
+import cfo from "../assets/alex.jpg";
+import sco from "../assets/sunny.png";
+import lco from "../assets/lco.jpeg";
 const Team = () => {
   return (
     <div className="min-h-[70vh]  bg-blue flex flex-col  md:items-center md:px-20 px-5 py-10">
@@ -28,6 +29,14 @@ const Team = () => {
           title="Chief Marketing Officer (CMO)"
           text="Maureen Dede is a dynamic marketing professional with over five years of experience. She specializes in brand strategy, digital marketing, and customer relationship management, passionately amplifying brand voices and connecting people to modern solutions."
         /> */}
+        <ObjCard
+          img={lco}
+          name="Olabowale Joseph Kunlere"
+          title="Legal Officer"
+          text="Olabowale Joseph Kunlere is a legal officer with a strong background in legal training and professional development. He holds an LLB from the University of Hull and an LL.M in Legal Practice from Nottingham Trent University. His experience includes a placement at Eversheds Sutherland LLP Nottingham in the Employment and Restructuring team, and he has previously taught tort law at Runshaw College-Leyland Campus, Preston.
+
+He completed his NYSC with Professor Yemi Akinseye-George SAN & Partners and has contributed to legal research and policy analysis for Nigeria’s criminal justice system through the Centre For Socio-Legal Studies. Olabowale brings a collaborative spirit and a commitment to supporting his colleagues, reflecting a proactive dedication to both personal growth and team development."
+        />
         <ObjCard
           img={cto}
           name="Idahtonye Toby"
